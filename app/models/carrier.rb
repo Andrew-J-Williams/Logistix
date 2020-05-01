562c259f9e8b5ad7754a1fd4d3bba253ab318165
Andrew-J-Williams <1williamsaj@gmail.com>
2020-04-30 20:29:01 -0500

    Established has_many/belongs_to relations

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,2 +1,4 @@
 class Carrier < ApplicationRecord
+    has_many :shipments
+    has_many :users, through: :shipments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :customers
+    has_many :shipments
+    has_many :carriers, through: :shipments
 end
