793e37d6502520dedb21077969b162f3b8b226e4
Andrew-J-Williams <1williamsaj@gmail.com>
2020-04-30 21:34:09 -0500

    Creating login and signup functionality, links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/' => "sessions#welcome" # We make a 'get' request that goes through our 'sessions' folder to access the 'welcome' file.
+  get '/login' => "sessions#new" # Brings the user to the login screen
+  post '/login' => "sessions#create" # Takes user information to generate a user session with their info.
+  get '/signup' => "users#new" # We don't need a 'post signup' because we already have a route in 'resources :users'
+  
+  
   resources :shipments
   resources :carriers
   resources :customers
---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_01_003235) do
+
+  create_table "carriers", force: :cascade do |t|
+    t.string "service_type"
+    t.integer "pending_shipments"
+    t.integer "delivered_shipments"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_customers_on_user_id"
+  end
+
+  create_table "shipments", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "total_weight"
+    t.date "ship_date"
+    t.integer "tracking_number"
+    t.string "status"
+    t.integer "user_id", null: false
+    t.integer "carrier_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["carrier_id"], name: "index_shipments_on_carrier_id"
+    t.index ["user_id"], name: "index_shipments_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "customers", "users"
+  add_foreign_key "shipments", "carriers"
+  add_foreign_key "shipments", "users"
+end
