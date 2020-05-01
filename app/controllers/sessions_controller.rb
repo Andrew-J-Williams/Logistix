207a8491b8d1383f167fda88147c14e9f40d6d81
Andrew-J-Williams <1williamsaj@gmail.com>
2020-04-30 22:21:20 -0500

    Developing sign up connections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
 
     def welcome
-        
+    end
+
+    def new
     end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        # Routes user to our 'sign up' screen.
+    end
+
 end
