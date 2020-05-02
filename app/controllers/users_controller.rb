217bfb522f6e848d20cf3cce036b4a1b25f26f2f
Andrew-J-Williams <1williamsaj@gmail.com>
2020-05-02 13:48:35 -0500

    Connecting paths, coding create and show methods for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,24 @@ class UsersController < ApplicationController
         # Routes user to our 'sign up' screen.
     end
 
+    def create
+        @user = User.new(user_params) #The user info from our "new.html.erb" view is posted here.
+        if @user.save # If our user validates correctly...
+            session[:user_id] = @user.id # We set our session's equal to the user's id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+
+    end
+
+    private
+
+    def user_params
+        params.require()
+    end
+
 end
