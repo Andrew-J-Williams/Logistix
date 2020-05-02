e4e1f0e8c2ef4811357590d0afd9384b2ffffb25
Andrew-J-Williams <1williamsaj@gmail.com>
2020-05-02 14:08:27 -0500

    Successfully created sign up components

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,26 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params) #The user info from our "new.html.erb" view is posted here.
+        @user = User.new(user_params) #The user info from our "new.html.erb" view is posted here. User_params is defined as a private method.
         if @user.save # If our user validates correctly...
             session[:user_id] = @user.id # We set our session's equal to the user's id
-            redirect_to user_path(@user)
-        else
-            render :new
+            redirect_to user_path(@user) # and then redirect this user to a different page.
+        else # But if our user does not validate...
+            render :new # they are sent back to the sign up page.
         end
     end
 
     def show
-
+        @user = User.find_by_id(params[:id]) # Checks for user id.
+        redirect_to '/' if !@user 
     end
 
     private
 
     def user_params
-        params.require()
+        params.require(:user).permit(:email, :username, :password)
+        # We first require our object that comes through our params...
+        # Then we permit any other params we desire. 
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_many :customers
     has_many :shipments
     has_many :carriers, through: :shipments
+
+    validates :email, uniqueness: true, presence: true
+    validates :username, uniqueness: true, presence: true
+
+    has_secure_password
+
 end
