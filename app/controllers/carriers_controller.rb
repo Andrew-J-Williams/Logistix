070abe07532541bf335def28c1b0687347b7b250
Andrew-J-Williams <1williamsaj@gmail.com>
2020-05-03 16:38:56 -0500

    Working on creating new Carriers/Services

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -1,10 +1,21 @@
 class CarriersController < ApplicationController
 
     def new
+        @carrier = Carrier.new
     end
 
     def create
+        @carrier = Carrier.new(carrier_params)
+        if @carrier.save
 
+        else
+            redirect_to new_carrier_path
+    end
+
+    private
+
+    def carrier_params
+        params.require(:carrier).permit(:name, :phone)
     end
 
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,9 @@
 class ServicesController < ApplicationController
+    def new
+
+    end
+
+    def create
+
+    end
 end
