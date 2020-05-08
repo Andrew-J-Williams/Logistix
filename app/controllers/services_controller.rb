da4571ce2bac7e11b3e10ba95ad688b9267ba45a
Andrew-J-Williams <1williamsaj@gmail.com>
2020-05-07 20:32:46 -0500

    Added 'index' and 'show' views to 'Service'

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,9 +1,29 @@
 class ServicesController < ApplicationController
+    def index
+        @services = Service.all
+    end
+    
     def new
         @service = Service.new
+        @service.build_carrier
     end
 
     def create
+        @service = Service.new(service_params)
+        if @service.save
+            redirect_to service_path(@service)
+        else
+            render :new
+        end
+    end
 
+    def show
+        @service = 
+    end
+
+    private
+
+    def service_params
+        params.require(:service).permit(:name, :carrier_id, carrier_attributes: [:name, :phone])
     end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,5 @@
 class Service < ApplicationRecord
   belongs_to :carrier
+
+  accepts_nested_attributes_for :carrier
 end
