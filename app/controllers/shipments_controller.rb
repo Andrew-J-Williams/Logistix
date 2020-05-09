beb2bb03241147a7f85002c5e4c9e833a8b580b4
Andrew-J-Williams <1williamsaj@gmail.com>
2020-05-09 14:47:02 -0500

    Connecting routes in Shipment

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,12 +1,28 @@
 class ShipmentsController < ApplicationController
 
     def new
-        Service.find_by_id(params[:id])
-        @shipment = @service.build_shipment
+        @service = Service.find_by_id(params[:service_id])
+        @shipment = @service.shipments.build
     end
 
     def create
+        @shipment = Shipment.new(shipment_params)
+        
+        if @shipment.save
+            redirect_to shipment_path(@shipment)
+        else
+            redirect_to :new
+        end
+    end
 
+    def index
+        @shipments = Shipment.all
+    end
+
+    private
+
+    def shipment_params
+        params.require(:shipment).permit(:customer, :address, :contact, :total_weight, :ship_date, :tracking_number ,:status, :service_id)
     end
 
 end
