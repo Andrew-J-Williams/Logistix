97d127713c50e4009f69ea6c7c4c86c9a8a4eb37
Andrew-J-Williams <1williamsaj@gmail.com>
2020-05-10 15:27:07 -0500

    Added nav bar in layouts with links

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -9,5 +9,13 @@ class Shipment < ApplicationRecord
   validates :ship_date, presence: true
   validates :tracking_number, presence: true
   validates :status, presence: true
+
+  def self.pending_shipments
+    where(status: "In-transit")
+  end
+
+  def self.delivered_shipments
+    where(status: "Delivered")
+  end
 end
 
